8036d2dac53dacb4b44b9c7b1163ce54d22e34b8
Meatlf <454779758@qq.com>
2020-05-02 11:03:23 +0800

    Added 实验题1:实现顺序栈的各种基本运算的算法.

---
+++ code/chapter03-栈和队列/exp3-1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "stack.h"
+
+using namespace std;
+using namespace sqstack;
+
+int main()
+{
+    cout << "实验题1:实现顺序栈的各种基本运算的算法." << endl;
+    cout << "sizeof(SqStack) = " << sizeof(SqStack) << endl;
+
+    ElemType str[4] = {'a', 'b', 'c', 'd'};
+
+    SqStack *st;
+
+    sqstack::InitStack(st);
+    cout << "栈是否为空,空则输出1,非空则输出0: " << sqstack::StackEmpty(st) << endl;
+
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        sqstack::Push(st, str[i]);
+        cout << "Pop栈中的元素: " << str[i++] << endl;
+    }
+    cout << "栈是否为空,空则输出1,非空则输出0: " << sqstack::StackEmpty(st) << endl;
+    ElemType e;
+    while (!sqstack::StackEmpty(st))
+    {
+        sqstack::Pop(st, e);
+        cout << "Pop栈中的元素: " << e << endl;
+    }
+    cout << "栈是否为空,空则输出1,非空则输出0: " << sqstack::StackEmpty(st) << endl;
+    sqstack::DestoryStack(st);
+    return 0;
+}
